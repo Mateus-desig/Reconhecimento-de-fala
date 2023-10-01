31f523e4083f9d0297f986839dfa7bfeee27991e
Mateus gonzaga <utracraft900@gmail.com>
2023-10-01 15:23:29 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+rec = sr.Recognizer()
+
+with sr.Microphone() as mic:
+    rec.adjust_for_ambient_noise(mic)
+    print("Pode falar que eu vou gravar")
+    audio = rec.listen(mic)
+    texto = rec.recognize_google(audio, language="pt-br")
+    print(texto)
